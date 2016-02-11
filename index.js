3f2a8af2b70a66a938127011f17b9502e25397a5
Ap0c <Ap0c@users.noreply.github.com>
2016-02-11 19:18:56 +0000

    Switched to using Omx class to handle player. Implemented control methods.

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ let spawn = require('child_process').spawn;
 
 // ----- Setup ----- //
 
+// The permitted audio outputs, local means via the 3.5mm jack.
 let ALLOWED_OUTPUTS = ['hdmi', 'local', 'both'];
 
 
 // ----- Functions ----- //
 
+// Creates an array of arguments to pass to omxplayer.
 function buildArgs (source, givenOutput) {
 
 	let output = '';
@@ -33,11 +35,46 @@ function buildArgs (source, givenOutput) {
 }
 
 
+// ----- Omx Class ----- //
+
 function Omx (source, output) {
 
 	let args = buildArgs(source, output);
 	let player = spawn('omxplayer', args);
+	let open = true;
 
-	return player;
+	player.on('close', () => {
+		open = false;
+	});
+
+	player.stdin.setEncoding('utf-8');
+
+	function writeStdin (value) {
+
+		if (open) {
+			player.stdin.write(value);
+		} else {
+			throw new Error('Player is closed.');
+		}
+
+	}
+
+	return {
+		play: () => { writeStdin('p'); },
+		pause: () => { writeStdin('p'); },
+		volUp: () => { writeStdin('+'); },
+		volDown: () => { writeStdin('-'); },
+		fwd30:() => { writeStdin('\u001b[C'); },
+		back30: () => { writeStdin('\u001b[D'); },
+		fwd600: () => { writeStdin('\u001b[A'); },
+		back600: () => { writeStdin('\u001b[B'); },
+		quit: () => { writeStdin('q'); },
+		get status () { return open; }
+	};
 
 }
+
+
+// ----- Module Exports ----- //
+
+module.exports = Omx;
